0116076f0ea9aca724596560aa1ccfb4b59d7082
dylanconklin <dylan.conklin@tutanota.com>
2023-12-07 14:43:10 -0800

    Add Type Annotations

--- map.py
+++ map.py
@@ -1,8 +1,8 @@
 import googlemaps
 from datetime import datetime
+from typing import Any
 
 async def maps() -> None:
     gmaps: googlemaps.Client = await googlemaps.Client(key="AIzaSyDlMzYGce3b7aTDSsKNEvdXe7Qf7ouoHMI")
-    current_location = gmaps.geolocate()
-    now = datetime.now()
-    directions_result = gmaps.directions("", "", mode="transit", departure_time=now)
+    now: datetime = datetime.now()
+    directions_result: Any = gmaps.directions("", "", mode="transit", departure_time=now)
--- tile.py
+++ tile.py
@@ -3,8 +3,8 @@ from pytile import async_login
 from pytile.tile import Tile
 from pytile.api import API
 
-async def get_known_tiles(userdata: str) -> {}:
-    tiles: {str : Tile} = {}
+async def get_known_tiles(userdata: str) -> dict[str : Tile]:
+    tiles: dict[str : Tile] = {}
     async with ClientSession() as session:
         api: API = await async_login(userdata["tile_username"], userdata["tile_password"], session)
         tiles: dict[str : Tile] = await api.async_get_tiles()
