1e83f5c3abc77f0155816b6671ed9843d0624325
dylanconklin <dylan.conklin@tutanota.com>
2023-12-07 14:45:53 -0800

    Run Formatter

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+from typing import Any
 import asyncio
 from timeit import default_timer
 import bleak
 from bleak import BLEDevice
 from pytile.tile import Tile
-from typing import Any
 
 import config
 import tile
 
+
 def time_to_seconds(time_hr: int = 0, time_min: int = 0, time_sec: int = 0) -> int:
     return (60 * ((time_hr * 60) + time_min)) + time_sec
+
 
 async def look_for_devices(time_limit: int, percentage: int, interval: int = 5):
     device_list_per_interval: [set(bleak.BLEDevice)] = []
@@ -33,6 +35,7 @@ async def look_for_devices(time_limit: int, percentage: int, interval: int = 5):
         print(d.details)
         print()
 
-config: dict[str : str | dict[str : Any]] = config.get_config()
-known_devices: dict[str : Tile] = asyncio.run(tile.get_known_tiles(config["user"]))
+
+config: dict[str : str | dict[str:Any]] = config.get_config()
+known_devices: dict[str:Tile] = asyncio.run(tile.get_known_tiles(config["user"]))
 asyncio.run(look_for_devices(1, 90))
--- map.py
+++ map.py
@@ -1,8 +1,13 @@
-import googlemaps
 from datetime import datetime
 from typing import Any
+import googlemaps
+
 
 async def maps() -> None:
-    gmaps: googlemaps.Client = await googlemaps.Client(key="AIzaSyDlMzYGce3b7aTDSsKNEvdXe7Qf7ouoHMI")
+    gmaps: googlemaps.Client = await googlemaps.Client(
+        key="AIzaSyDlMzYGce3b7aTDSsKNEvdXe7Qf7ouoHMI"
+    )
     now: datetime = datetime.now()
-    directions_result: Any = gmaps.directions("", "", mode="transit", departure_time=now)
+    directions_result: Any = gmaps.directions(
+        "", "", mode="transit", departure_time=now
+    )
