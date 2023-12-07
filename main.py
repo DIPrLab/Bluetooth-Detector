bd25f7a42d4d13063e87c728795ba416e829f4c2
dylanconklin <dylan.conklin@tutanota.com>
2023-12-07 14:38:56 -0800

    Add Type Annotations, Comments

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import asyncio
 from timeit import default_timer
 import bleak
+from bleak import BLEDevice
+from pytile.tile import Tile
+from typing import Any
 
 import config
 import tile
@@ -10,20 +13,26 @@ def time_to_seconds(time_hr: int = 0, time_min: int = 0, time_sec: int = 0) -> i
 
 async def look_for_devices(time_limit: int, percentage: int, interval: int = 5):
     device_list_per_interval: [set(bleak.BLEDevice)] = []
-    start_time = default_timer()
+    results: set(BLEDevice) = set()
+    start_time: float = default_timer()
+
+    # Scan for devices (within given time frame) and add to list, divided by interval
     while (default_timer() - start_time) < time_limit:
-        devices = await bleak.BleakScanner.discover(timeout=interval)
+        devices: list[BLEDevice] = await bleak.BleakScanner.discover(timeout=interval)
         device_list_per_interval.append(set(devices))
-    results: set(bleak.BLEDevice) = set()
-    for go in device_list_per_interval:
-        for device in go:
+
+    # Add all scanned devices into set, without regard for time
+    for devices in device_list_per_interval:
+        for device in devices:
             results.add(device)
+
+    # Print device info for all scanned devices
     print("Devices:")
     for d in results:
         print(d.name, d.address)
         print(d.details)
         print()
 
-config = config.get_config()
-known_devices = asyncio.run(tile.get_known_tiles(config["user"]))
+config: dict[str : str | dict[str : Any]] = config.get_config()
+known_devices: dict[str : Tile] = asyncio.run(tile.get_known_tiles(config["user"]))
 asyncio.run(look_for_devices(1, 90))
